e556cfc988e5b4fd07d8957176c1640808fbc80d
Manuel Fontan <manfontan@mail.me>
2021-12-15 18:06:01 +0000

    Add merge trees function and tests
    Signed-off-by: Manuel Fontan <manfontan@mail.me>

--- algorithmsI/tree/tree.go
+++ algorithmsI/tree/tree.go
@@ -30,3 +30,28 @@ func insertSliceToTree(n **TreeNode, slice []interface{}, index int) {
 	insertSliceToTree(&((*n).Left), slice, 2*index+1)
 	insertSliceToTree(&((*n).Right), slice, 2*index+2)
 }
+
+// You are given two binary trees root1 and root2.
+// Imagine that when you put one of them to cover the other,
+// some nodes of the two trees are overlapped while the others are not.
+// You need to merge the two trees into a new binary tree.
+// The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node.
+// Otherwise, the NOT null node will be used as the node of the new tree.
+// Return the merged tree.
+// Note: The merging process must start from the root nodes of both trees.
+func MergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+
+	if root1 == nil {
+		return root2
+	}
+
+	if root2 == nil {
+		return root1
+	}
+
+	root1.Val += root2.Val
+	root1.Left = MergeTrees(root1.Left, root2.Left)
+	root1.Right = MergeTrees(root1.Right, root2.Right)
+
+	return root1
+}
--- algorithmsI/tree/tree_test.go
+++ algorithmsI/tree/tree_test.go
@@ -28,3 +28,26 @@ func TestSliceToTree(t *testing.T) {
 		}
 	}
 }
+
+func TestMergeTrees(t *testing.T) {
+
+	type testCase struct {
+		root1, root2, want *tree.TreeNode
+	}
+
+	testCases := []testCase{
+		{
+			root1: tree.SliceToTree([]interface{}{1, 2, 3, 4, 5}),
+			root2: tree.SliceToTree([]interface{}{2, 3, 4, 5, 6}),
+			want:  tree.SliceToTree([]interface{}{3, 5, 7, 9, 11}),
+		},
+	}
+
+	for _, tc := range testCases {
+		got := tree.MergeTrees(tc.root1, tc.root2)
+
+		if !tree.IsSameTree(got, tc.want) {
+			t.Errorf("got %v, want %v", *got, tc.want)
+		}
+	}
+}
