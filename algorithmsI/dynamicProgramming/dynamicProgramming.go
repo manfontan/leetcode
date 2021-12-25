e7c9258c932f3e04260b9571fa041be8615156e0
Manuel Fontan <manfontan@mail.me>
2021-12-25 23:24:56 +0100

    add dynamic programming module. Add update matrix function and tests
    Signed-off-by: Manuel Fontan <manfontan@mail.me>

---
+++ algorithmsI/dynamicProgramming/dynamicProgramming.go
@@ -0,0 +1,55 @@
+package dynamicProgramming
+
+// Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+// The distance between two adjacent cells is 1.
+// Time complexity: O(r⋅c)
+// We perform two passes over the matrix and each pass requires O(r⋅c) time.
+// Space complexity: O(1)
+func UpdateMatrix(m [][]int) [][]int {
+	r := len(m)
+	c := len(m[0])
+	dist := make([][]int, r)
+
+	if r == 0 {
+		return m
+	}
+
+	for i := 0; i < r; i++ {
+		dist[i] = make([]int, c)
+		for j := 0; j < c; j++ {
+			dist[i][j] = r * c
+			if m[i][j] == 0 {
+				dist[i][j] = 0
+			}
+			if i > 0 {
+				dist[i][j] = min(dist[i][j], dist[i-1][j]+1)
+			}
+			if j > 0 {
+				dist[i][j] = min(dist[i][j], dist[i][j-1]+1)
+			}
+		}
+	}
+
+	for i := r - 1; i >= 0; i-- {
+		for j := c - 1; j >= 0; j-- {
+			if m[i][j] == 0 {
+				dist[i][j] = 0
+			}
+			if i > r-1 {
+				dist[i][j] = min(dist[i][j], dist[i+1][j]+1)
+			}
+			if j > c-1 {
+				dist[i][j] = min(dist[i][j], dist[i][j+1]+1)
+			}
+		}
+	}
+
+	return dist
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
